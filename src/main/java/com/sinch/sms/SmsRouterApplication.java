a63c0a800d2f91e6b870652a370352d1bfd3515d
Chenglin Jing <chenglinjing@Chenglins-MacBook-Air.local>
2025-06-01 18:29:37 +0800

    Initial commit

---
+++ src/main/java/com/sinch/sms/SmsRouterApplication.java
@@ -0,0 +1,13 @@
+package com.sinch.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsRouterApplication.class, args);
+	}
+
+}
